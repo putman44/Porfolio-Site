f2ab20856fdfd1d3d2d1f167a085abdf0816434e
Taylor Putman <fishputman@gmail.com>
2026-02-10 06:37:25 -0700

    edited trivia game description

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -37,7 +37,7 @@ const projects: Project[] = [
     id: 3,
     title: "Interactive-Trivia-SPA",
     description:
-      "The Trivia Quiz App is a dynamic, interactive quiz platform built with React. Users can select a category and difficulty, answer multiple-choice questions, and view their results at the end. The app demonstrates proficiency with modern React features, including hooks, useReducer for state management, contextual state lifting, API integration, and localStorage persistence.",
+      "Interactive Trivia SPA is a dynamic, interactive quiz platform built with React. Users can select a category and difficulty, answer multiple-choice questions, and view their results at the end. The app demonstrates proficiency with modern React features, including hooks, useReducer for state management, contextual state lifting, API integration, and localStorage persistence.",
     image: "OpenTriviaDatabase.png",
     tags: ["React", "Javascript", "useReducer"],
     demoUrl: "https://open-trivia-database-quiz-app.vercel.app/",
